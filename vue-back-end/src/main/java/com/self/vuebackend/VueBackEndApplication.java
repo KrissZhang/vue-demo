dc696b2fb3ec2c009455a4764cb3da0b166e4ccc
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2020-05-18 14:47:12 +0800

    vue-demo
    vue-demo init

---
+++ vue-back-end/src/main/java/com/self/vuebackend/VueBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.self.vuebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBackEndApplication.class, args);
+    }
+
+}
---
+++ vue-back-end/src/test/java/com/self/vuebackend/VueBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.vuebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
